0d78e7afb726ae897d557b862f2fa53b70c012c4
Neotter <tinkleidea@hotmail.com>
2023-06-28 16:21:38 +0800

    ADD MultiCall

---
+++ 0x027_MultiCall/contracts/MultiCall.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MultiCall {
+    // Call command structure
+    struct Call {
+        address target;
+        bool allowFailure;
+        bytes callData;
+    }
+
+    // Result structure
+    struct Result {
+        bool success;
+        bytes returnData;
+    }
+
+    function multicall(
+        Call[] calldata calls
+    ) public returns (Result[] memory returnData) {
+        uint256 length = calls.length;
+        returnData = new Result[](length);
+        Call calldata callInstance;
+
+        // start multicall
+        for (uint i = 0; i < length; i++) {
+            Result memory result = returnData[i];
+            callInstance = calls[i];
+            (result.success, result.returnData) = callInstance.target.call(callInstance.callData);
+            if(!(callInstance.allowFailure||result.success)){
+                revert("Multicall: call failed.");
+            }
+        }
+    }
+}
